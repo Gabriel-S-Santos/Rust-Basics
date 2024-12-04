d4e0380d4ed8df919c5acb4ee167a9fe32e1e343
Gabriel-S-Santos <gabrielsilvadossantos@protonmail.com>
2024-12-04 11:58:48 -0300

    Closures

---
+++ closures/target/debug/closures.d
@@ -0,0 +1,1 @@
+/home/santos/RustWeb3/closures/target/debug/closures: /home/santos/RustWeb3/closures/src/main.rs
---
+++ closures/target/debug/deps/closures-31bc479897ee7b69.d
@@ -0,0 +1,5 @@
+/home/santos/RustWeb3/closures/target/debug/deps/closures-31bc479897ee7b69: src/main.rs
+
+/home/santos/RustWeb3/closures/target/debug/deps/closures-31bc479897ee7b69.d: src/main.rs
+
+src/main.rs:
