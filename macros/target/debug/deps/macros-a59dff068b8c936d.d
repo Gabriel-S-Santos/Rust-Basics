323c8bf5e9bf3c5225437a165c3a9802799fd90c
Gabriel-S-Santos <gabrielsilvadossantos@protonmail.com>
2024-12-04 17:06:48 -0300

    Using macros

---
+++ macros/target/debug/deps/macros-a59dff068b8c936d.d
@@ -0,0 +1,5 @@
+/home/santos/RustWeb3/macros/target/debug/deps/macros-a59dff068b8c936d: src/main.rs
+
+/home/santos/RustWeb3/macros/target/debug/deps/macros-a59dff068b8c936d.d: src/main.rs
+
+src/main.rs:
---
+++ macros/target/debug/macros.d
@@ -0,0 +1,1 @@
+/home/santos/RustWeb3/macros/target/debug/macros: /home/santos/RustWeb3/macros/src/main.rs
